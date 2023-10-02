8e4bf6f0af31f3d0a1ec8ef2259787cf2a31eeeb
alexeyk500 <alexeyk500@yandex.ru>
2023-10-02 19:52:12 +0300

    db connected and work var with db and client

--- src/app/api/user-list/route.ts
+++ src/app/api/user-list/route.ts
@@ -1,7 +1,10 @@
 import { sql } from '@vercel/postgres';
+import { db } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const users = await sql`SELECT * FROM Users;`;
+  // const users = await sql`SELECT * FROM Users;`;
+  const client = await db.connect();
+  const users = await client.sql`SELECT * FROM Users;`;
   return NextResponse.json({ users, status: 200 });
 }
